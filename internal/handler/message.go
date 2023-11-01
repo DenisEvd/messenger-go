9e118f17d2e746553c5adeeb38c0f30612130e9a
DenisEvd <den-evdokimov-2015@mail.ru>
2023-11-01 17:04:12 +0300

    feat: get handler

--- internal/handler/message.go
+++ internal/handler/message.go
@@ -5,12 +5,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"messenger-go/domain"
 	"net/http"
+	"strconv"
 )
 
 func (h *Handler) sendMessage(c *gin.Context) {
 	var message domain.Message
 	if err := c.ShouldBindJSON(&message); err != nil {
-		newErrorResponse(c, http.StatusBadRequest)
+		newErrorResponse(c, http.StatusBadRequest, "invalid message json")
 		return
 	}
 
@@ -20,5 +21,17 @@ func (h *Handler) sendMessage(c *gin.Context) {
 }
 
 func (h *Handler) getMessages(c *gin.Context) {
+	senderID, err := strconv.Atoi(c.Param("sender_id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid or not found sender id")
+	}
 
+	receiverID, err := strconv.Atoi(c.Param("sender_id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid or not found receiver id")
+	}
+
+	fmt.Println(senderID, receiverID)
+
+	c.JSON(http.StatusOK, "")
 }
